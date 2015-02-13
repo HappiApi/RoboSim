8f9763e862b692e33a6cb8ef37fb05c14604a7e3
HappiApi <apinanhasthanasombat@Apinans-MacBook-Air.local>
2015-02-13 15:55:36 +0000

    Submission Done
    Unless anything broke, this is being submitted !

---
+++ Sheet 3/Pair22_Blackwell_Hasthanasombat/Task3.1/Task3.1.c
@@ -0,0 +1,176 @@
+// Pair22_Blackwell_Hasthanasombat
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include "picomms.h"
+#include "calcPos.h"
+
+#define distanceToWall 25
+#define distanceToFront 25
+#define endOfWallMultiplier 2
+#define slowSpeed 30 //15
+#define fastSpeed 50 //30
+#define superSpeed 70 //50
+#define frontIROffset 4
+#define followSide LEFT
+#define threshold 5
+
+int leftEnc,rightEnc;
+
+void setSensors()
+{
+	set_ir_angle(LEFT, -45);
+	set_ir_angle(RIGHT,-75);
+}
+
+void calcP()
+{
+	get_motor_encoders(&leftEnc,&rightEnc);
+	calcPosition(leftEnc,rightEnc);
+	reset_motor_encoders();
+}
+
+void checkWallAhead() {
+  int usDist = get_us_dist();
+
+  if (usDist < 20 && get_front_ir_dist(RIGHT) < 20) {
+    while (usDist > 15) {
+      set_motors(10,10);
+      usDist = get_us_dist();
+    }
+    printf("STOPPED, terminate program.");
+    while(1){
+      set_motors(0,0);
+	}
+  }
+}
+
+void adjustParallel()
+{
+	if (get_side_ir_dist(followSide) > get_front_ir_dist(followSide)-frontIROffset)		
+	{
+		set_motors(fastSpeed,slowSpeed);
+		printf("RIGHT SL%d FL%d\n", get_side_ir_dist(followSide), get_front_ir_dist(followSide));
+	}
+
+	if(get_side_ir_dist(followSide) < get_front_ir_dist(followSide)-frontIROffset)
+	{
+		set_motors(slowSpeed,fastSpeed);
+		printf("LEFT SL%d FL%d\n", get_side_ir_dist(followSide), get_front_ir_dist(followSide));
+	}	
+}
+
+void lookForFront()
+{
+	if(get_front_ir_dist(RIGHT) < distanceToFront)
+	{
+		while(get_front_ir_dist(RIGHT) < distanceToFront)
+		{
+			set_motors(fastSpeed,-fastSpeed);
+			printf(" FRONT FR%d\n",get_front_ir_dist(RIGHT) );
+
+			checkWallAhead();
+			calcP();
+		}
+	}
+}
+
+void lookForEnd()
+{
+	if( endOfWallMultiplier * get_side_ir_dist(followSide) < get_front_ir_dist(followSide)-frontIROffset)
+	{
+		while(endOfWallMultiplier * get_side_ir_dist(followSide) < get_front_ir_dist(followSide))
+		{
+			set_motors(slowSpeed,superSpeed);
+			printf("UTURN SL%d FL%d\n", get_side_ir_dist(followSide), get_front_ir_dist(followSide) );
+
+			checkWallAhead();
+
+			calcP();
+		}
+	}
+}
+
+void straight()
+{
+	if( get_side_ir_dist(followSide) == get_front_ir_dist(followSide)-frontIROffset)
+	{
+		set_motors(fastSpeed,fastSpeed);
+		printf("STRAIGHT SL%d FL%d\n", get_side_ir_dist(followSide), get_front_ir_dist(followSide));
+	}
+}
+
+/*void adjustSide()
+{
+	if(get_side_ir_dist(followSide) - distanceToWall > threshold || get_front_ir_dist(followSide) - distanceToWall > threshold)
+	{
+		while(get_side_ir_dist(followSide) - distanceToWall || get_front_ir_dist(followSide) < distanceToWall)
+		{
+			set_motors(fastSpeed,slowSpeed);
+			printf(" SIDE OUT %d %d\n", get_side_ir_dist(LEFT), get_front_ir_dist(LEFT));
+		}
+	}
+
+	if(get_side_ir_dist(followSide) > distanceToWall || get_front_ir_dist(followSide) > distanceToWall)
+	{
+		while(get_side_ir_dist(followSide) > distanceToWall || get_front_ir_dist(followSide) > distanceToWall)
+		{
+			set_motors(slowSpeed,fastSpeed);
+			printf(" SIDE IN %d %d\n", get_side_ir_dist(LEFT), get_front_ir_dist(LEFT));
+		}
+	}
+}*/
+
+void adjustSide()
+{
+	if(abs(get_front_ir_dist(followSide) - distanceToWall) > threshold && abs(get_side_ir_dist(followSide) - distanceToWall) > threshold) // check over threshold
+	{
+		if(get_front_ir_dist(followSide) - distanceToWall < 0 && get_side_ir_dist(followSide) - distanceToWall < 0)	//case turn right
+		{
+			while(abs(get_front_ir_dist(followSide) - distanceToWall) > threshold && abs(get_side_ir_dist(followSide) - distanceToWall) > threshold)
+			{
+				set_motors(fastSpeed,slowSpeed);
+				printf(" SIDE OUT %d %d\n", get_side_ir_dist(LEFT), get_front_ir_dist(LEFT));
+
+				checkWallAhead();
+
+				calcP();
+			}
+		}
+
+		if(get_front_ir_dist(followSide) - distanceToWall > 0 && get_side_ir_dist(followSide) - distanceToWall > 0) //case turn left
+		{
+			while(abs(get_front_ir_dist(followSide) - distanceToWall) > threshold && abs(get_side_ir_dist(followSide) - distanceToWall) > threshold)
+			{
+				set_motors(slowSpeed,fastSpeed);
+				printf(" SIDE IN %d %d\n", get_side_ir_dist(LEFT), get_front_ir_dist(LEFT));
+
+				checkWallAhead();
+
+				calcP();
+			}
+		}
+	}
+}
+
+
+
+int main()
+{
+	connect_to_robot();
+	initialize_robot();
+	setSensors();
+
+	while(1)
+	{
+		adjustParallel();
+		lookForFront();
+		lookForEnd();
+		straight();
+		adjustSide();
+		checkWallAhead();
+		calcP();
+
+	}
+}
---
+++ Sheet 3/Pair22_Blackwell_Hasthanasombat/Task3.1/calcPos.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <math.h>
+
+#define encToMillimetres 1000/956	
+#define widthOfRobot 236		// In mm
+
+double angleTurned;		// current turning angle
+double prevAngle = 0;		// initial condition previous angle = 0
+double deltaX, deltaY;
+double Rl, Rr, Rm;
+double leftEncInMillimetres, rightEncInMillimetres;
+double p, q, a, b ; 	// Should probably find better name
+
+struct coordinates 
+{
+  double x;
+  double y;
+};
+
+struct coordinates coords; 
+
+void convertEncToMM(int leftEnc, int rightEnc)
+	{
+		leftEncInMillimetres = (double)leftEnc * encToMillimetres ;
+		rightEncInMillimetres = (double)rightEnc * encToMillimetres ;
+	}
+
+void calcParameters()
+	{
+		angleTurned = ( leftEncInMillimetres - rightEncInMillimetres ) / widthOfRobot ;
+		Rl = leftEncInMillimetres / angleTurned ;
+		Rr = rightEncInMillimetres / angleTurned ;
+		Rm = ( Rl + Rr ) / 2 ;
+	}
+
+void calcStraightDist()	// case where angleTurned == 0
+{
+	coords.x += rightEncInMillimetres * sin(prevAngle) ;	
+	coords.y += rightEncInMillimetres * cos(prevAngle) ;
+}
+
+//When robot !at 0 rad (given previous angle phi)  // <-- General case ?
+void calcDeltas()
+	{
+		p = Rm * sin(prevAngle + angleTurned) ;
+		q = Rm * sin(prevAngle) ;
+		deltaY = p - q ;
+
+		a = Rm - ( Rm * cos(prevAngle + angleTurned) ) ;
+		b = Rm - ( Rm * cos(prevAngle) ) ;
+		deltaX = a - b ;
+	}
+
+//Add deltaX and deltaY to cumulative position
+
+void addDeltasToCumulative()
+	{
+		coords.x += deltaX ;
+		coords.y += deltaY ;
+	}
+
+//Adjust current angle to previous angle
+
+void changePrevAngle()
+	{
+		prevAngle = angleTurned ;	//adjusted not += 
+	}
+
+void calcPosition(int leftEnc, int rightEnc) 
+{
+	convertEncToMM(leftEnc, rightEnc);
+	if (fabs(leftEncInMillimetres - rightEncInMillimetres) < 1.0)
+	{
+		calcStraightDist();
+	}
+	else
+	{
+		calcParameters();
+		calcDeltas();
+		addDeltasToCumulative();
+		changePrevAngle();
+		set_origin();
+		set_point((int)(coords.x / encToMillimetres), (int)(coords.y / encToMillimetres));
+	}
+	printf("X : %f Y : %f Angle : %f\n", coords.x, coords.y, angleTurned * 180 / 3.141592);
+						
+/*
+When robot at 0 rad [Eg. Starting] use 
+	{
+		deltaY = Rm * math.sin(angleTurned) ;
+		deltaX = Rm - ( Rm * math.cos(angleTurned) ) ;
+	}
+*/
+}
+
