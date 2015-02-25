9b4a59b4e30bf843f9ba732e07e32ce0bc26491d
DanBlackwell <danblackwell95@yahoo.co.uk>
2015-02-25 18:24:27 +0000

    Added working backtracker.

--- Sheet4/allInOne.c
+++ Sheet4/allInOne.c
@@ -6,51 +6,137 @@
 #include "picomms.h"
 //#include "calcPos.h"
 
-#define followSpeed 35 //15
-#define frontIROffset 4
-#define followSide LEFT
-#define thresholdDist 35.0
+#define FOLLOW_SPEED 35 //15
+#define FOLLOW_BK_SPD 15
+#define THRESHOLD_DIST 35.0
 #define MIN_TURN_RADIUS 25
-#define rightAngleTurnTarget 210
-#define fastestNonSlipMotorValue 15
-#define topTurnSpeed 40
+#define RIGHT_ANGLE_TICKS 210
+#define NO_SLIP_TURN_SPD 15
+#define TOP_TURN_SPEED 40
 #define LEFT 0
 #define RIGHT 1
-#define encToCentimetres (9.55 * 3.141592 / 360)
-#define widthOfRobot 22.5   // In cm
+#define ENC_TO_CM (9.55 * 3.141592 / 360)
+#define ROBOT_WIDTH 22.5   // In cm
 
 struct coordinates 
 {
   double x;
   double y;
-  // int lMSpeed;
-  // int rMSpeed;
+  int position;
   struct coordinates * next;
 };
 
 typedef struct coordinates coords;
 
-coords* head = NULL; coords* currentNode;
+coords* head = NULL; coords* currentNode; int nodePosition = 1;
 
-int leftEnc; int rightEnc; int leftEncPrev; int rightEncPrev; double xCurrent; double yCurrent;
+int i = 0; double xCurrent, yCurrent;
 double prevAngle = 0;   // initial condition previous angle = 0
 double xCoord = 0; double yCoord = 0;
 
-void calcPosition(int leftEnc, int rightEnc) 
+/*finds the angle between the heading direction and a point*/
+double angleToTargetPoint(coords * target) {
+  double angleFromY, deltaX, deltaY, angleFromCurrent;// diagDist, aheadDist, acrossDist;
+
+  deltaX = target->x - xCurrent;
+  deltaY = target->y - yCurrent;
+
+  angleFromY = fabs(atan(deltaX / deltaY));
+  if (deltaX > 0) {
+    angleFromY = M_PI - angleFromY; 
+  } else {
+    angleFromY = M_PI + angleFromY;
+  } 
+  angleFromCurrent = angleFromY - prevAngle;
+
+  printf("\n\ndelta x : %f, delta y : %f, angleFromY : %f, angleFromCurrent : %f",deltaX, deltaY, angleFromY * 180 / M_PI, angleFromCurrent * 180 / M_PI);
+
+  return angleFromCurrent * 180 / M_PI;
+}
+
+/*finds the distance between the robot and a point*/
+double calcDistToPoint (coords * temp) {
+  double deltaX, deltaY, distToPoint;
+
+  deltaX = temp->x - xCurrent;
+  deltaY = temp->y - yCurrent; 
+  distToPoint = sqrt(deltaX * deltaX + deltaY * deltaY);
+
+  return distToPoint;
+}
+
+/*finds the furthest coordinate ahead within a set radius (target) */
+coords * findTarget () {
+  coords * temp = currentNode;
+  double target = 20;
+  int removedBehind = 0;
+  double distToNode, closestNodeDist = 30.0;
+  
+    while(1) {
+      distToNode = calcDistToPoint(temp);
+      if (distToNode < closestNodeDist) {
+        currentNode = temp;
+        closestNodeDist = distToNode;
+      }
+      if (temp->position == 1) {  //don't allow it to try searching beyond the end of the list
+        return temp;
+      }
+      if (distToNode < target && calcDistToPoint(temp->next) > target && temp->y < yCurrent) {  //if this is too cpu heavy for the RasPi then change calcDistToPoint(temp->next) to calcDistToPoint(temp->next->next->next) and adjust 3 lines below as noted 
+        return temp;
+      } else {
+        temp = temp->next;  //change this to temp->next->next->next as well!
+      }
+    }
+}
+
+/*Function that handles the following back of a linked list of coordinates*/
+void followBack() {
+  coords * target;
+  int i;
+  double angleToTarget;
+  double ratio = 0.03;
+  while(1) {
+
+    target = findTarget();
+    if (currentNode-> position < 3) {
+      set_motors(0,0);
+      exit(0);
+    }
+
+    printf("\n\nfollow back, position %i, target pos: %i", currentNode->position, target->position);
+    
+    angleToTarget = angleToTargetPoint(target);
+
+    printf("\n\n%f", angleToTarget);
+    if (angleToTarget > 0) {
+      set_motors(FOLLOW_BK_SPD, (int)((1 - ratio * angleToTarget) * (float)FOLLOW_BK_SPD));
+    } else {
+      set_motors((int)((1.0 - (ratio * -angleToTarget)) * FOLLOW_BK_SPD), FOLLOW_BK_SPD);
+    }
+
+    calcPosition();
+
+    usleep(10000);
+
+  }
+}
+
+void calcPositionMaths(int leftEnc, int rightEnc) 
 {
   double angleTurned;   // current turning angle
   double deltaX, deltaY;
   double Rl, Rr, Rm;
   double p, q, a, b ;   // Should probably find better name
 
+
   if (fabs(leftEnc - rightEnc) < 1)
   {
-    xCoord += (double)leftEnc * encToCentimetres * sin(prevAngle) ; 
-    yCoord += (double)rightEnc * encToCentimetres * cos(prevAngle) ;
+    xCoord += (double)leftEnc * ENC_TO_CM * sin(prevAngle) ; 
+    yCoord += (double)rightEnc * ENC_TO_CM * cos(prevAngle) ;
   }
   else
   {
-    angleTurned = (double)( leftEnc - rightEnc ) * encToCentimetres / widthOfRobot ;
+    angleTurned = (double)( leftEnc - rightEnc ) * ENC_TO_CM / ROBOT_WIDTH ;
     Rl = (double)leftEnc / angleTurned ;
     Rr = (double)rightEnc / angleTurned ;
     Rm = ( Rl + Rr ) / 2.0 ;
@@ -63,11 +149,17 @@ void calcPosition(int leftEnc, int rightEnc)
     b = Rm * cos(prevAngle + angleTurned) ;
     deltaX = a - b ;
 
-    xCoord += deltaX * encToCentimetres ;
-    yCoord += deltaY * encToCentimetres ;
-    prevAngle += angleTurned ;  //adjusted to += 
+    xCoord += deltaX * ENC_TO_CM ;
+    yCoord += deltaY * ENC_TO_CM ;
+    if (prevAngle + angleTurned < 0) {
+      prevAngle = 2 * M_PI + prevAngle + angleTurned;
+    } else if (prevAngle + angleTurned > 2 * M_PI) {
+      prevAngle = prevAngle + angleTurned - 2 * M_PI;
+    } else {
+      prevAngle += angleTurned ;  //adjusted to += 
+    }
   }
-  log_trail();
+  // log_trail();
   xCurrent = xCoord;
   yCurrent = yCoord;
   set_point(round(xCurrent), round(yCurrent)); //set point in centimetres
@@ -75,46 +167,42 @@ void calcPosition(int leftEnc, int rightEnc)
   printf("Dist Travelled: %imm, angle travelled at: %f degrees\n", (int)sqrt((xCurrent * xCurrent) + (yCurrent * yCurrent)), atan(xCurrent / yCurrent) * 180 / 3.141592); 
 }
 
-void followBack() {
-  float angleToNext;
-  float xDist; float yDist;
-  float distToNext;
-
-  xDist = currentNode->x - currentNode->next->x;
-  yDist = currentNode->y - currentNode->next->y;
-
-  angleToNext = atan(xDist/yDist);
-  printf("currentNode->x: %f, currentNode->next->x: %f, angle to next: %f\n",currentNode->x, currentNode->next->x, angleToNext * 180 / 3.141592);
-  if (angleToNext>0) {
-    turn(angleToNext, LEFT);
+/*apply data smoothing to the encoder readings within this function*/
+void calcPosition() {
+  static int lEnc[3], rEnc[3], lEncFinal, rEncFinal; static int leftEncPrev = 0; static int rightEncPrev = 0;
+  if (i % 3 == 0) {
+    get_motor_encoders(&lEnc[0], &rEnc[0]);
+  } else if (i % 3 == 1) {
+    get_motor_encoders(&lEnc[1], &rEnc[1]);
   } else {
-    turn(-angleToNext, RIGHT);
+    get_motor_encoders(&lEnc[2], &rEnc[2]);
   }
-
-  distToNext = sqrt(xDist * xDist + yDist * yDist);
-
-  get_motor_encoders(&leftEnc, &rightEnc);
-  while (leftEnc < distToNext) {
-    set_motors(15,15);
-    get_motor_encoders(&leftEnc, &rightEnc);
-  }
+  if (i > 1) {
+    lEncFinal = round((float)(lEnc[0] + lEnc[1] + lEnc[2]) / 3.0);
+    rEncFinal = round((float)(rEnc[0] + rEnc[1] + rEnc[2]) / 3.0);
+    calcPositionMaths(lEncFinal - leftEncPrev, rEncFinal - rightEncPrev);
+    leftEncPrev = lEncFinal;
+    rightEncPrev = rEncFinal;
+  } 
+  i++;
 }
 
 void turn(float targetAngle, int direction) 
 {
-  int slowSpd = fastestNonSlipMotorValue;
-  int fastSpd = topTurnSpeed - fastestNonSlipMotorValue;
+  int slowSpd = NO_SLIP_TURN_SPD;
+  int fastSpd = TOP_TURN_SPEED - NO_SLIP_TURN_SPD;
   int currentSpd;
   double percentTurned = 0;
-  int leftenc = 0;
-  int rightenc = 0;
-  int target = (double)(rightAngleTurnTarget * targetAngle) / 90;
+  int leftenc, lEncInitial;
+  int rightenc, rEncInitial;
+  int target = (double)(RIGHT_ANGLE_TICKS * targetAngle) / 90;
 
-  reset_motor_encoders();
+  get_motor_encoders(&lEncInitial, &rEncInitial);
+  rightenc = rEncInitial; leftenc = lEncInitial;
 
-  while(fabs(leftenc)<target && fabs(rightenc)<target) {
+  while(fabs(lEncInitial - leftenc)<target && fabs(rEncInitial - rightenc)<target) {
     get_motor_encoders(&leftenc,&rightenc);
-    percentTurned = fabs(leftenc) / (double)target;
+    percentTurned = fabs(leftenc - lEncInitial) / (double)target;
     if (percentTurned <= 0.5) {
       currentSpd = slowSpd + (int)(2 * percentTurned * (double)fastSpd);
     } else {
@@ -125,37 +213,30 @@ void turn(float targetAngle, int direction)
     } else {
       set_motors(-currentSpd, currentSpd);
     }
-    // printf("%d %d\n",leftenc,rightenc);
-    log_trail();
   }
 
   set_motors(0,0);
   usleep(40000);
 }
 
-void comeToStop(coords * head, coords * currentNode) {
+void comeToStop() {
   while (get_front_ir_dist(RIGHT) > 15) {
     set_motors(15,15);
   }
   set_motors(0,0);
-  get_motor_encoders(&leftEnc,&rightEnc);
-  calcPosition(leftEnc,rightEnc);
+  calcPosition();
   currentNode = (coords *)malloc(sizeof(coords)); //creates new node
   currentNode->x = xCurrent; //assigns value of x and y coords for new node
   currentNode->y = yCurrent;
+  currentNode->position = nodePosition;
   currentNode->next = head;
   head = currentNode; //changes the head of the list to be the new node
+  nodePosition++;
 
   currentNode = head;
   turn(180, LEFT);
-  int i = 1;
-  while (currentNode) {
-    printf("%i. x : %f, y : %f\n", i, currentNode->x, currentNode->y);
-    followBack();
-    currentNode = currentNode->next;
-    i++;
-  }
-  exit(0);
+  calcPosition();
+  followBack();
 }
 
 int main() {
@@ -163,43 +244,40 @@ int main() {
 	initialize_robot();
   set_origin();
   set_ir_angle(RIGHT, -45);
-  int i = 0;
 
   int wallDist;
   float turnRate = 0;
-  float multiplierL = 0.5 * followSpeed;
-  float multiplierR = followSpeed;
+  float multiplierL = 0.5 * FOLLOW_SPEED;
+  float multiplierR = FOLLOW_SPEED;
 
   usleep(10000);
 
   while (1) {
   	wallDist = get_front_ir_dist(LEFT);
     if (get_front_ir_dist(RIGHT) < 25) {
-      comeToStop(head, currentNode);
+      comeToStop();
     }
-  	if (wallDist == thresholdDist) {
-  		set_motors(followSpeed, followSpeed);
-  	} else if (wallDist < thresholdDist) {
-  		turnRate = 1.0 - (0.8 * wallDist / thresholdDist);
-  		set_motors(followSpeed, (int)(followSpeed - multiplierR * turnRate));  //Turn right
+  	if (wallDist == THRESHOLD_DIST) {
+  		set_motors(FOLLOW_SPEED, FOLLOW_SPEED);
+  	} else if (wallDist < THRESHOLD_DIST) {
+  		turnRate = 1.0 - (0.8 * wallDist / THRESHOLD_DIST);
+  		set_motors(FOLLOW_SPEED, (int)(FOLLOW_SPEED - multiplierR * turnRate));  //Turn right
   	} else {
       if (wallDist > 70) {
         turnRate = 1.0;
       } else {
-  	   	turnRate = 1.5 * (wallDist / thresholdDist - 1.0);
+  	   	turnRate = 1.5 * (wallDist / THRESHOLD_DIST - 1.0);
   		}
-      set_motors((int)(followSpeed - multiplierL * turnRate), followSpeed);
+      set_motors((int)(FOLLOW_SPEED - multiplierL * turnRate), FOLLOW_SPEED);
   	}
-
-      get_motor_encoders(&leftEnc,&rightEnc);
-      calcPosition(leftEnc - leftEncPrev,rightEnc - rightEncPrev);
-      leftEncPrev = leftEnc;
-      rightEncPrev = rightEnc;
+      calcPosition();
       currentNode = (coords *)malloc(sizeof(coords)); //creates new node
       currentNode->x = xCurrent; //assigns value of x and y coords for new node
       currentNode->y = yCurrent;
+      currentNode->position = nodePosition;
       currentNode->next = head;  //makes the new node point to the previous one
       head = currentNode; //changes the head of the list to be the new node
+      nodePosition++;
   }
 
 }
